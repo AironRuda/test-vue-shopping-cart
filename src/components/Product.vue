6fb116fe5fe6df695be01ad7f653f32feb650ddc
AironRuda <airon.ruda@udea.edu.co>
2023-04-13 23:08:08 -0500

    feat: better product cards

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { store } from "../store.js";
 import Swal from "sweetalert2";
+import { errorAlert, successAlert } from "../utilities/alert";
 
 const products = store.stock;
 const cart = store.cart;
@@ -8,26 +9,13 @@ const cart = store.cart;
 const addCart = (product) => {
   const productCart = { ...product, quantity: 0 };
   if (cart.some((cartProduct) => cartProduct.name == product.name)) {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "El articulo ya está en el carrito!",
-    });
+    errorAlert("El articulo ya está en el carrito!");
   } else {
     if (product.stock == 0) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "No hay invenatio de este producto",
-      });
+      errorAlert("No hay inventario de este producto");
     } else {
       store.addToCart(productCart);
-      Swal.fire({
-        icon: "success",
-        title: "Articulo agregado al carrito",
-        showConfirmButton: false,
-        timer: 1500,
-      });
+      successAlert("Articulo agregado al carrito");
     }
   }
 };
@@ -36,33 +24,45 @@ const addCart = (product) => {
 <template>
   <div class="w-[100%] p-3 m-3 md:w-[50%]">
     <h1 class="font-bold text-[60px] text-center">Product list</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2">
-      <div v-for="product in products" class="bg-slate-400 m-2 p-2 rounded-md">
-        <div class="flex justify-center">
-          <img
-            :src="product.img"
-            :alt="product.name"
-            class="rounded-md h-[300px] md:h-[150px]"
-          />
-        </div>
-        <div class="flex flex-col items-center border-t-2 border-black m-2">
-          <p class="font-bold text-[30px] md:text-[20px]">
-            {{ product.name }}
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div
+        v-for="product in products"
+        class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      >
+        <a href="#">
+          <img class="rounded-t-lg" :src="product.img" alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              {{ product.name }}
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Articulos en inventario {{ product.stock }}
           </p>
-          <p class="my-2">unit price: ${{ product.unit_price }}</p>
-          <div class="flex border-2 border-black">
-            <p
-              class="flex items-center border-r-2 border-black px-4 h-[25px] md:h-[50px]"
+          <a
+            href="#"
+            @click="addCart(product)"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Agregar al carrito
+            <svg
+              aria-hidden="true"
+              class="w-4 h-4 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              {{ product.stock }}
-            </p>
-            <button
-              class="bg-blue-500 px-4 h-[25px] md:h-[50px]"
-              @click="addCart(product)"
-            >
-              add to cart
-            </button>
-          </div>
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </a>
         </div>
       </div>
     </div>
