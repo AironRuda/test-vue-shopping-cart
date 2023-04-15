4f4c8ab6d5f16172b6e53308002e9ec9dd2a2154
AironRuda <airon.ruda@udea.edu.co>
2023-04-15 12:56:21 -0500

    feat: Header

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import Slider from "./components/Slider.vue";
 import ProductList from "./components/ProductList.vue";
 import CartList from "./components/CartList.vue";
 import { store } from "./store";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
+  <Header />
   <div class="bg-sky-400">
     <Slider />
-    <div class="flex flex-col-reverse justify-start md:flex-row">
+    <div class="flex flex-col-reverse justify-center md:flex-row">
       <ProductList />
       <!-- <CartList /> -->
       <CartList v-if="store.cart.length != 0" />
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1
+    class="flex justify-center items-center h-24 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+  >
+    <span
+      class="underline underline-offset-3 decoration-8 decoration-sky-400 dark:decoration-blue-600"
+      >Alternova shop</span
+    >
+  </h1>
+</template>
